7438d4113b3bc4ec48d90de318e1d7c41324e826
Nani Hutagaol <nanihutagaol@gmail.com>
2018-11-06 17:35:03 +0700

    commit for issue 1
    do not directly use repo,, use service before

--- inventory_management/src/main/java/future/inventory_management/Controller/InventoryController.java
+++ inventory_management/src/main/java/future/inventory_management/Controller/InventoryController.java
@@ -2,6 +2,7 @@ package future.inventory_management.Controller;
 
 import future.inventory_management.Model.Inventory;
 import future.inventory_management.Repository.InventoryRepository;
+import future.inventory_management.Service.InventoryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,24 +14,46 @@ import java.util.List;
 @RequestMapping("/inv_mgt/inventory")
 public class InventoryController {
     @Autowired
-    private InventoryRepository inventoryRepository;
+    private InventoryService inventoryService;
 
-    //create inventory
-    @PostMapping("/create")
-    public Inventory createInventory(@Valid @RequestBody Inventory inventory){
-        return inventoryRepository.save(inventory);
+    @GetMapping("/test")
+    public String testView(){
+        return "index";
     }
 
-    //get all
     @GetMapping("/getAll")
     public List<Inventory> getAllInventory(){
-        return inventoryRepository.findAll();
+        return inventoryService.getAllInventory();
     }
 
-//    //get by id
-//    @GetMapping("/getById/{id}")
-//    public Inventory getById(@PathVariable ObjectId _invId){
-//        return inventoryRepository._findById(_invId);
-//    }
+    @GetMapping("/getById/{id}")
+    public Inventory getById(@PathVariable("id") String invId){
+        return inventoryService.getInventoryById(invId);
+    }
+
+    @PostMapping("/create")
+    public Inventory create(@Valid @RequestBody Inventory inventory){
+        return inventoryService.createInventory(inventory);
+    }
+
+    @PutMapping(value = "/updateById/{id}")
+    public Inventory updateById(@PathVariable("id") String invId, @Valid @RequestBody Inventory inventory) {
+        inventory.set_id(invId);
+        inventoryService.updateInventoryById(invId, inventory);
+        return inventory;
+    }
+
+    public String updateStockById(){
+        return "";
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public int deleteById(@PathVariable("id") String _invId) {
+        inventoryService.deleteInventoryById(_invId);
+        return 1;
+    }
+
+
+
 
 }
